60e585f32cdd9e4e9ff7a420d3064cff4d4dfdb7
2019331101-Tanjumul <2019331101-Tanjumul>
2025-11-03 14:28:23 +0600

    variables , unicode and characters

---
+++ main.py
@@ -0,0 +1,53 @@
+print("tanjumul test")
+print("python-creed")
+
+num = [2 , 34, 333, 22 ,36,77, 8,5]
+num.sort()
+print(num); 
+
+
+cal = [12 ,45, 88, 123,00,12]
+
+cal.sort()
+
+print(cal) 
+
+#this is a comment
+#for multiline use dockstring
+
+"""Hellow this is a multiline commnets"""
+
+tanjumul = 33; 
+print(tanjumul) 
+
+TanjumulAlom = "pascle-case"
+
+    
+tanjumulAlom = " camel-case" 
+
+tanjumul_alom = "tanjumul snake" 
+
+print(tanjumul_alom)
+
+a =-133 #integer i can say 
+print(a)
+
+c = 12/3 #this is a fraction point number. now it will be using the float datatype. 
+print(type(c))
+
+ca = 3.23 #float option
+print(type(ca))
+
+com = 34j #there was a concept of iota , i which was imaginary value.
+print(type(com))
+
+st = 'this is tanjumul, pretending as Batman using a string' #this is the stringline
+
+print(st)
+
+str2 = 't'
+print(ord(str2))
+
+b = 65 #this is the unicode that prints this number to character. now use chr 
+
+print(chr(b))
