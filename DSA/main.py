c59d3033952d06ea759a290f3c2fd407db10fea6
2019331101-Tanjumul <crackerroot4@gmail.com>
2025-11-04 01:40:49 +0600

    input and output

--- DSA/main.py
+++ DSA/main.py
@@ -83,3 +83,14 @@ a = 55
 a = str(a)
 print(a)
 print(type(a))
+
+
+
+print("hello how are you? ")
+name = "tanjumul"
+age = "26"
+
+print(name,age)
+print(name + age) 
+
+
---
+++ DSA/tempCodeRunnerFile.py
@@ -0,0 +1,14 @@
+
+a = 55 
+
+a = str(a)
+print(a)
+print(type(a))
+
+
+
+print("hello how are you? ")
+name = "tanjumul"
+age = "26"
+
+print(name,age) 
