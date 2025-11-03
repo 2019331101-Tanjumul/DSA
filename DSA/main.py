769856d3a1956def569918210708897d6f4816e8
2019331101-Tanjumul <crackerroot4@gmail.com>
2025-11-03 15:13:31 +0600

    Add actual DSA files

---
+++ DSA/main.py
@@ -0,0 +1,63 @@
+print("tanjumul test")
+print("python-creed")
+
+num = [2 , 34, 333, 22 ,36,77, 8,5]
+num.sort()
+print(num); 
+
+
+cal = [12 ,45, 88, 123,00,12]
+
+cal.sort()
+
+print(cal) 
+
+#this is a comment
+#for multiline use dockstring
+
+"""Hellow this is a multiline commnets"""
+
+tanjumul = 33; 
+print(tanjumul) 
+
+TanjumulAlom = "pascle-case"
+
+    
+tanjumulAlom = " camel-case" 
+
+tanjumul_alom = "tanjumul snake" 
+
+print(tanjumul_alom)
+
+a =-133 #integer i can say 
+print(a)
+
+c = 12/3 #this is a fraction point number. now it will be using the float datatype. 
+print(type(c))
+
+ca = 3.23 #float option
+print(type(ca))
+
+com = 34j #there was a concept of iota , i which was imaginary value.
+print(type(com))
+
+st = 'this is tanjumul, pretending as Batman using a string' #this is the stringline
+
+print(st)
+
+str2 = 't'
+print(ord(str2))
+
+b = 65 #this is the unicode that prints this number to character. now use chr 
+
+print(chr(b))
+
+A =  'tanjumul Alom'
+
+print(A[3])
+
+str4 = 'committhrough your thoughts'
+print(str4[8])
+str8 = 'thiss is fun'
+print(str8[0])
+print("something is good")
