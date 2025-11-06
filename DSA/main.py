34f10dafd60592a59caa5a5f37b6bb488dbd77c3
2019331101-Tanjumul <crackerroot4@gmail.com>
2025-11-06 19:12:44 +0600

    opertior and condition

--- DSA/main.py
+++ DSA/main.py
@@ -1,115 +1,115 @@
-print("tanjumul test")
-print("python-creed")
+# print("tanjumul test")
+# print("python-creed")
 
-num = [2 , 34, 333, 22 ,36,77, 8,5]
-num.sort()
-print(num); 
+# num = [2 , 34, 333, 22 ,36,77, 8,5]
+# num.sort()
+# print(num); 
 
 
-cal = [12 ,45, 88, 123,00,12]
+# cal = [12 ,45, 88, 123,00,12]
 
-cal.sort()
+# cal.sort()
 
-print(cal) 
+# print(cal) 
 
-#this is a comment
-#for multiline use dockstring
+# #this is a comment
+# #for multiline use dockstring
 
-"""Hellow this is a multiline commnets"""
+# """Hellow this is a multiline commnets"""
 
-tanjumul = 33; 
-print(tanjumul) 
+# tanjumul = 33; 
+# print(tanjumul) 
 
-TanjumulAlom = "pascle-case"
+# TanjumulAlom = "pascle-case"
 
     
-tanjumulAlom = " camel-case" 
+# tanjumulAlom = " camel-case" 
 
-tanjumul_alom = "tanjumul snake" 
+# tanjumul_alom = "tanjumul snake" 
 
-print(tanjumul_alom)
+# print(tanjumul_alom)
 
-a =-133 #integer i can say 
-print(a)
+# a =-133 #integer i can say 
+# print(a)
 
-c = 12/3 #this is a fraction point number. now it will be using the float datatype. 
-print(type(c))
+# c = 12/3 #this is a fraction point number. now it will be using the float datatype. 
+# print(type(c))
 
-ca = 3.23 #float option
-print(type(ca))
+# ca = 3.23 #float option
+# print(type(ca))
 
-com = 34j #there was a concept of iota , i which was imaginary value.
-print(type(com))
+# com = 34j #there was a concept of iota , i which was imaginary value.
+# print(type(com))
 
-st = 'this is tanjumul, pretending as Batman using a string' #this is the stringline
+# st = 'this is tanjumul, pretending as Batman using a string' #this is the stringline
 
-print(st)
+# print(st)
 
-str2 = 't'
-print(ord(str2))
+# str2 = 't'
+# print(ord(str2))
 
-b = 65 #this is the unicode that prints this number to character. now use chr 
+# b = 65 #this is the unicode that prints this number to character. now use chr 
 
-print(chr(b))
+# print(chr(b))
 
-A =  'tanjumul Alom'
+# A =  'tanjumul Alom'
 
-print(A[3])
+# print(A[3])
 
-str4 = 'committhrough your thoughts'
-print(str4[8])
-str8 = 'thiss is fun'
-print(str8[0])
-print("something is good")
+# str4 = 'committhrough your thoughts'
+# print(str4[8])
+# str8 = 'thiss is fun'
+# print(str8[0])
+# print("something is good")
 
-print('after another landed0')
-
-
-
-str14 = 'finetuning the machine learning models'
-
-print(str14[16]) 
-
-cc = str14[16]
-
-print(ord(cc))
-
-str10 = 'Tanjumul Alom Siam is here '
-print(str10[0:7:1])
-
-
-a = 55 
-
-a = str(a)
-print(a)
-print(type(a))
+# print('after another landed0')
 
 
 
-print("hello how are you? ")
-name = "tanjumul"
-age = "26"
+# str14 = 'finetuning the machine learning models'
 
-print(name,age)
-print(name + age) 
+# print(str14[16]) 
 
-a_name = "Tanvir Rahman Soad"
-a_age = 23 
+# cc = str14[16]
 
-print(f"My name is {name} and my age is {age}, i got a brother his name {a_name} and age {a_age}")
+# print(ord(cc))
 
-
-# input("What is your age ? ")
+# str10 = 'Tanjumul Alom Siam is here '
+# print(str10[0:7:1])
 
 
-tage = int(input("What is your actual age ? "))
+# a = 55 
+
+# a = str(a)
+# print(a)
+# print(type(a))
+
+
+
+# print("hello how are you? ")
+# name = "tanjumul"
+# age = "26"
+
+# print(name,age)
+# print(name + age) 
+
+# a_name = "Tanvir Rahman Soad"
+# a_age = 23 
+
+# print(f"My name is {name} and my age is {age}, i got a brother his name {a_name} and age {a_age}")
+
+
+# # input("What is your age ? ")
+
+
+# tage = int(input("What is your actual age ? "))
+
+# # print(type(tage))
+# print(tage)
 
 # print(type(tage))
-print(tage)
-
-print(type(tage))
-#but to change the type i have to do : i have to put int() while declaring in the input variable in the top
+# #but to change the type i have to do : i have to put int() while declaring in the input variable in the top
 
 
 
-
+print(100 != 300 and 25>11 and 93<1 )
