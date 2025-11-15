36b9ceed6ee1f56ee70291cf9032aec5310209e6
2019331101-Tanjumul <crackerroot4@gmail.com>
2025-11-16 01:37:39 +0600

    This is the factors of a number

--- DSA/.github/main.py
+++ DSA/.github/main.py
@@ -513,16 +513,27 @@
 
 
 
-n = int(input("Enter the number of range you want to give me : "))
+# n = int(input("Enter the number of range you want to give me : "))
 
-even = 0
-odd = 0 
+# even = 0
+# odd = 0 
 
-for i in range(0,n+1): 
-    if i%2==0:
-        even+=i
-    else : 
-        odd+=i
+# for i in range(0,n+1): 
+#     if i%2==0:
+#         even+=i
+#     else : 
+#         odd+=i
 
-print(f"The even number of sum is {even}")
-print(f"The odd number of sum is {odd}")
+# print(f"The even number of sum is {even}")
+# print(f"The odd number of sum is {odd}")
+
+#taking the factors of a number : 
+
+n = int(input("Please give me the factor of the number you are wanting : "))
+
+for i in range(1,n+1): 
+    if n%i==0:
+        print(i)
+       
+    else:
+       continue
