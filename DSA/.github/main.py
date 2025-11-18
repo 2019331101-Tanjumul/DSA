effbe597a5fee150947852f7380e59b7e5db23e7
2019331101-Tanjumul <crackerroot4@gmail.com>
2025-11-18 20:55:23 +0600

    it will add the new int in the last of the list : methods of list

--- DSA/.github/main.py
+++ DSA/.github/main.py
@@ -739,9 +739,9 @@
 
 #list :; 
 
-a = [13, 14 , 144 , 1111, 345]
+# a = [13, 14 , 144 , 1111, 345]
 
-fruits = ['apple', 'banana']
+# fruits = ['apple', 'banana']
 
 # print(fruits[0])
 # print(a[3])
@@ -750,11 +750,21 @@ fruits = ['apple', 'banana']
 
 # print(a[0:4:1])
 
-for i in range(len(a)): 
-    print(a[i])
+# for i in range(len(a)): 
+#     print(a[i])
 
 #second way : 
 
 # for i in a: 
 #     print(i)
 
+
+
+##method : is defined in a class : 
+
+# print(dir(list))
+
+l = [1,2,3,3,3,4,5,6,7,88,9,0,0,44,3,34,43,3,434,34,34,5,6,6]
+
+l.append(11)
+print(l)
